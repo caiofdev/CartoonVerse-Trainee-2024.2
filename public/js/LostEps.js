d83192b63cc0ad376c3868d3e193abc5b573e9a9
Kadusilva <dasilvaolliveira.carloseduardo@gmail.com>
2024-10-31 20:04:15 -0300

    Lost-eps feito

---
+++ public/js/LostEps.js
@@ -0,0 +1,15 @@
+ep = document.getElementById(ep).value
+
+function Abrirlosteps() {
+  window.open("lost-eps.html", "_self")
+}
+
+function Fecharlosteps() {
+  window.open("login.html", "_self")
+}
+
+function Losteps() {
+  if (ep = 'biruta') {
+    window.open("https://youtu.be/rklpHFbLABg?si=l0xRNLK_OVHidCzl", "_self")
+  }
+}
--- public/js/text-shadow.js
+++ public/js/text-shadow.js
@@ -1,9 +1,10 @@
 function trocarShadow() {
-  var titulo = document.getElementById("titulo")
-  if ((titulo.style.textShadow = "2px 2px #13ACB1")) {
-    titulo.style.textShadow = "2px 2px #7E241E"
+  titulo = document.getElementById("titulo");
+  px = '2px';
+  if ((titulo.style.textShadow.value = "2px 2px #13ACB1")) {
+    titulo.style.textShadow.value = px + px + '#7E241E';
   }
-  if ((titulo.style.textShadow = "2px 2px #7E241E")) {
-    titulo.style.textShadow = "2px 2px #13ACB1"
+  if ((titulo.style.textShadow.value = "2px 2px #7E241E")) {
+    titulo.style.textShadow.value = "2px 2px #13ACB1";
   }
 }
