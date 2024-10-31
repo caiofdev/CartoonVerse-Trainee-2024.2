d9adb9eaa175ec97ef45723dfaf75b0e4142caee
Rafael Rocha <faelrribeiro3@gmail.com>
2024-10-31 16:26:52 -0300

    ADICIONANDO MENU DE HAMBURGUER

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
-document.getElementById('arrow').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
+document.getElementById("arrow").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.toggle("open-sidebar")
+})
+
+const sb = document.getElementById("sidebar")
+const sbc = document.getElementById("sidebar-content")
+document.getElementById("hamburguer").addEventListener("click", function () {
+  if (sb.style.backgroundColor !== "transparent") {
+    sb.style.backgroundColor = "transparent"
+    sbc.style.display = "none"
+    document.getElementById("sidebar").classList.remove("open-sidebar")
+  } else {
+    document.getElementById("sidebar").classList.add("open-sidebar")
+    sb.style.backgroundColor = "var(--fundo)"
+    sbc.style.display = "flex"
+  }
+})
+
+/* const viewportWidth = window.innerWidth
+const viewportHeight = window.innerHeight
+
+if ((window.innerWidth) >= 769){
+   document.getElementById("sidebar").classList.add("open-sidebar")
+   sb.style.backgroundColor = "var(--fundo)"
+   sbc.style.display = "flex"
+}
+if ((window.innerWidth) < 769) {
+      sb.style.backgroundColor = "transparent"
+      sbc.style.display = "none"
+      document.getElementById("sidebar").classList.remove("open-sidebar")
+} */
