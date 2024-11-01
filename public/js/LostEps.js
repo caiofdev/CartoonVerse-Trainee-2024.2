951694ce7ffd2f7700fd52f9a0fb032e89800247
Kadusilva <dasilvaolliveira.carloseduardo@gmail.com>
2024-11-01 16:50:46 -0300

    Epic

--- public/js/LostEps.js
+++ public/js/LostEps.js
@@ -9,13 +9,59 @@ function Fecharlosteps() {
 }
 
 function Losteps() {
-  if (ep = 'biruta') {
+  //rachador
+  if ((ep = "biruta")) {
     window.open("https://youtu.be/rklpHFbLABg?si=l0xRNLK_OVHidCzl", "_self")
   }
+  //yabadabadooo
   if ((ep = "yabadabadooo")) {
     window.open("https://youtu.be/qpGx4foRdPw?si=lV3fis_yQxZOYmlz", "_self")
   }
-  if (ep = "mago") {
+  //contemplem o mago
+  if ((ep = "mago")) {
     window.open("https://youtu.be/I0nYyNxm8Ew?si=3bjpUjMsheeVYVT9", "_self")
   }
+  //dangerous
+  if ((ep = "hermes")) {
+    window.open(
+      "https://open.spotify.com/intl-pt/track/2sxpbOUUSzRrms8vSZwxvo?si=ffbe1ca2348541e3",
+      "_self"
+    )
+  }
+  //Epic
+  if ((ep = "athena")) {
+    window.open(
+      "https://open.spotify.com/playlist/3HvgaZeBWbr7UjFeicPFRI?si=VtDOEEA0TQG3fFI9V3W-Sw&pi=AZ5z8RnMQ0-0b",
+      "_self"
+    )
+  }
 }
+
+// Scooby doo
+// Jetson
+// tvz Três espiãs d+
+// x-men evolution
+// padrinhos mágicos
+// coragem o cão covarde
+// As aventuras de Jack-chan
+// Pokemon
+// Dragon Ball
+// Yu-Gi-Oh!
+// Avatar
+// Liga da Justiça
+// du dudu e edu
+// billy e mandy
+// doug funnie
+// jhony bravo
+// KND turma do bairro
+// Digimon
+// Danny Phantom
+// Samurai Jack
+// EPIC (AUREA)
+// trigemeas
+// Capitão Planeta 
+// Saint Seiya
+// Os gárgulas
+// castelo rá-tim-bum
+// 
+//
