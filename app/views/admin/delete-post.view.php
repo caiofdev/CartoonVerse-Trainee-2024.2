99bea6aedd2eec232495ead9820427cc791cce23
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2024-12-06 14:59:07 -0300

    ajusta cor e escrita do modal de exclusao

--- app/views/admin/delete-post.view.php
+++ app/views/admin/delete-post.view.php
@@ -10,7 +10,7 @@
     <div id="modalDeletePost-<?= $post->id ?>" class="modal-delete-post">
       <div class="modal-content-delete-post">
         <a class="close-delete-post" onclick="fecharModal('modalDeletePost-<?= $post->id ?>')">&times;</a>
-        <h1>Tem certeza que deseja excluir o post?</h1>
+        <h1>Tem certeza que deseja<br>excluir este post?</h1>
         <div class="botoes-delete-post">
           <button class="fechar-delete-post" type="button" onclick="fecharModal('modalDeletePost-<?= $post->id ?>')">Cancelar</button>
           <form action="delete-post" method="POST" class="modal-form">
