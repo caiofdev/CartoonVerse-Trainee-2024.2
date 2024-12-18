afced276e689048758fba0594cd34f9737e1838e
Kadusilva <dasilvaolliveira.carloseduardo@gmail.com>
2024-12-18 07:40:02 -0300

    fix padronizei o alinhamento dos headers da post-list e da user-list

--- app/views/admin/post-list.view.php
+++ app/views/admin/post-list.view.php
@@ -18,7 +18,6 @@
       <div id="header">
 
       <?php require __DIR__ . '/sidebar.view.php' ?>
-      <div></div>
         <!-- Titulo -->
         <h1>LISTA DE POSTS</h1>
         <button id="novo" onclick="abrirModal('modalCriarPost')">
